15ccae4c027fcd38bc6d19decd862ef5438cbfb1
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2025-10-17 15:25:51 +0800

    Update main.py

--- Binary_Search/Search_2D_Matrix/main.py
+++ Binary_Search/Search_2D_Matrix/main.py
@@ -1,13 +1,34 @@
 #Brute Force Method O(n)
 from typing import List
 class Solution:
+    ##def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    ##    for i in range(len(matrix)):
+    ##        n = matrix[i]
+    ##        for j in range(len(n)):
+    ##            if (n[j] == target):
+    ##                return (True)
+    ##     return (False)
+    # Using Binary Sort O(logm*n)
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            n = matrix[i]
-            for j in range(len(n)):
-                if (n[j] == target):
-                    return (True)
-        return (False)
+        if not matrix or not matrix[0]:
+                    return False
+        R = len(matrix)
+        C = len(matrix[0]) 
+        low = 0
+        high = R * C - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            i = mid // C
+            j = mid % C
+            mid_value = matrix[i][j] 
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                low = mid + 1
+            else:
+                high = mid - 1       
+        return False
 
                 
 c = Solution()
